e17a80f1ae575e645fbad96f6fdd5e58053102c5
Tessa Azar MacLean <tessaamac@gmail.com>
2021-05-26 18:47:38 +0000

    Add a TS version

---
+++ winners-and-losers/viaTs.ts
@@ -0,0 +1,42 @@
+// change variable and function names due to linter and viaJs.js
+
+interface IGame {
+  winner: string, 
+  loser: string, 
+  losersScore: number
+}
+
+const gameArrTs = [
+  { winner: 'Alice', loser: 'Bob', losersScore: 2 },
+  { winner: 'Alice', loser: 'Dean', losersScore: 3 },
+  { winner: 'Elise', loser: 'Bob', losersScore: 1 },
+  { winner: 'Elise', loser: 'Carol', losersScore: 4 },
+  { winner: 'Carol', loser: 'Dean', losersScore: 3 }
+];
+
+function getPlayersTs(gameArr: IGame[]) {
+  const playerArr = [];
+  for (const game of gameArr) {
+    for (const [key, name] of Object.entries(game)) {
+      // check specifically for 'winner' and 'loser' in case more keys are 
+      // added to the gameArr later as future-proofing
+      if ((key === 'winner' || key === 'loser') && !playerArr.includes(name)) {
+        playerArr.push(name);
+      }
+    }
+  }
+  return playerArr;
+};
+
+function whoBeatWhoTs(gameArr: IGame[]) {
+  const newObj = {};
+  for (const player of getPlayers(gameArr)) {
+    newObj[player] = [];
+  }
+  for (const game of gameArr) {
+    newObj[game.winner].push(game.loser);
+  }
+  return newObj;
+};
+
+console.log(whoBeatWho(gameArr));
