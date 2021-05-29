e0f3761310526325442415daf95ca2941fdacd26
Tessa Azar MacLean <74215019+TeyyaM@users.noreply.github.com>
2021-05-28 17:44:38 -0700

    Add ruby version

--- pig-latin/via_ruby.rb
+++ pig-latin/via_ruby.rb
@@ -1,3 +1,12 @@
 require 'pp'
-userInput = ARGV;
-pp(userInput)
+# Uses ruby 2.6 endless range
+string = ''
+for i in 0 ... ARGV.length
+    string += ARGV[i][1..]
+    string += "#{ARGV[i][0]}ay"
+    if i < ARGV.length - 1
+      string += ' '
+    end
+end
+
+pp(string)
